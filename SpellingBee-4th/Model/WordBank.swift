b88bbf4f963ce1c0bb9d93d0178cb36d6694b76f
Eric Hernandez <eands9@yahoo.com>
2019-01-16 08:51:25 -0600

    greek6

--- SpellingBee-4th/Model/WordBank.swift
+++ SpellingBee-4th/Model/WordBank.swift
@@ -12,15 +12,15 @@ class WordBank{
     var list = [Word]()
     
     init(){
-        list.append(Word(word:"hydraulic", sentence:""))
-        list.append(Word(word:"trauma", sentence:""))
-        list.append(Word(word:"hygiene", sentence:""))
-        list.append(Word(word:"semantics", sentence:""))
-        list.append(Word(word:"thesaurus", sentence:""))
-        list.append(Word(word:"phenomenon", sentence:""))
-        list.append(Word(word:"cosmos", sentence:""))
-        list.append(Word(word:"protagonist", sentence:""))
-        list.append(Word(word:"acronym", sentence:""))
-        list.append(Word(word:"paradox", sentence:""))
+        list.append(Word(word:"synchronous", sentence:""))
+        list.append(Word(word:"misanthropy", sentence:""))
+        list.append(Word(word:"sarcasm", sentence:""))
+        list.append(Word(word:"ephemeral", sentence:""))
+        list.append(Word(word:"polygon", sentence:""))
+        list.append(Word(word:"nemesis", sentence:""))
+        list.append(Word(word:"syntax", sentence:""))
+        list.append(Word(word:"eureka", sentence:""))
+        list.append(Word(word:"topography", sentence:""))
+        list.append(Word(word:"panic", sentence:""))
     }
 }
